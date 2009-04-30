65c34d28944859f6c8274c0f502337733d5a22ba
erik sohnel <dfw407so@dd.bib.de>
2009-04-30 15:32:24 +0200

    scratch file commit

--- hoeck/rel/play.clj
+++ hoeck/rel/play.clj
@@ -91,9 +91,10 @@ B -> #{{:vorname diana, :name kirsch} {:vorname robert, :name schmock}}
 {:adress-id 14, :vorname 16, :name 18, :id 5}
 
 (def ttt (make-relation
-#{{:a 1 :b 2}
-  {:a 1 :b 1}
-  {:a 2 :b 2}}))
+#{{:a 1 :b 2 :c 1}
+  {:a 1 :b 1 :c 3}
+  {:a 2 :b 2 :c 3}}))
+(def ittt (index ttt))
 
 
 {:a {2 #{{:a 2, :b 2}}, 1 #{{:a 1, :b 1} {:a 1, :b 2}}}, :b {2 #{{:a 1, :b 2} {:a 2, :b 2}}, 1 #{{:a 1, :b 1}}}}
@@ -117,20 +118,161 @@ B -> #{{:vorname diana, :name kirsch} {:vorname robert, :name schmock}}
 {2 #{{:a 2, :b 2}}, 1 #{{:a 1, :b 1} {:a 1, :b 2}}}
 
 
+(require 'clojure.contrib.pprint)
+ittt
+(clojure.contrib.pprint/pprint '{:a {2 #{{:a 2, :b 2, :c 3}}, 1 #{{:a 1, :b 2, :c 1} {:a 1, :b 1, :c 3}}}, :c {3 #{{:a 2, :b 2, :c 3} {:a 1, :b 1, :c 3}}, 1 #{{:a 1, :b 2, :c 1}}}, :b {1 #{{:a 1, :b 1, :c 3}}, 2 #{{:a 1, :b 2, :c 1} {:a 2, :b 2, :c 3}}}})
+{:a
+ {2 #{{:c 3, :b 2, :a 2}}, 1 #{{:c 1, :b 2, :a 1} {:c 3, :b 1, :a 1}}},
+ :c
+ {3 #{{:c 3, :b 2, :a 2} {:c 3, :b 1, :a 1}}, 1 #{{:c 1, :b 2, :a 1}}},
+ :b
+ {1 #{{:c 3, :b 1, :a 1}}, 2 #{{:c 1, :b 2, :a 1} {:c 3, :b 2, :a 2}}}}
+
+(ittt :a)
+{2 #{{:a 2, :b 2, :c 3}}, 1 #{{:a 1, :b 2, :c 1} {:a 1, :b 1, :c 3}}}
+(ittt :b)
+{1 #{{:a 1, :b 1, :c 3}}, 2 #{{:a 1, :b 2, :c 1} {:a 2, :b 2, :c 3}}}
+(ittt :c)
+{3 #{{:a 2, :b 2, :c 3} {:a 1, :b 1, :c 3}}, 1 #{{:a 1, :b 2, :c 1}}}
+
+(defn group-by-lookup [i field-name
+  (let [i ittt
+      field-names '(:a :b)
+      ii (map i field-names)]
+  (map (fn [[k v]
+            
+            (i field-name)))
+  
+(doc lazy-seq)
+-------------------------
+clojure.core/lazy-seq
+([& body])
+Macro
+  Takes a body of expressions that returns an ISeq or nil, and yields
+  a Seqable object that will invoke the body only the first time seq
+  is called, and will cache the result and return it on all subsequent
+  seq calls. Any closed over locals will be cleared prior to the tail
+  call of body.
+
+
+
+{2 #{{:a 2, :b 2, :c 3}}, 1 #{{:a 1, :b 2, :c 1} {:a 1, :b 1, :c 3}}}
+{1 #{{:a 1, :b 1, :c 3}}, 2 #{{:a 1, :b 2, :c 1} {:a 2, :b 2, :c 3}}}
+#{1 2}
+{2 #{{:a 2, :b 2, :c 3}}, 1 #{{:a 1, :b 2, :c 1} {:a 1, :b 1, :c 3}}}
+{1 #{{:a 1, :b 1, :c 3}}, 2 #{{:a 1, :b 2, :c 1} {:a 2, :b 2, :c 3}}}
+#{nil}
 
 
 
 
 
+(map #(((index ttt) :b) (:b %)) ((index ttt) :a))
 
 
 
 
+(2 1)
+
+;map:
+([f coll]
+   (lazy-seq
+    (when-let [s (seq coll)]
+      (cons (f (first s)) (map f (rest s))))))
+
+(defn extract-fields-from-index [index index-values fields]
+  (mapcat #(map (fn [tup] (select-keys tup (conj fields index-key))) (val %)) (index index-key)))
+
+(extract-fields-from-index ittt :a :b :c)
+({:b 2, :a 2} {:b 2, :a 1} {:b 1, :a 1})
 
 
+(defn group-by-keys [i field-names]
+  (let [ks (apply extract-fields-from-index i field-names)]
+
+(let [i ittt
+      field-names '(:a :b)]
+
+(defn index-part [lookup-fn, tuple-seq, fieldname]
+  (magic-map (fn ([] (map fieldname tuple-seq))
+                 ([k] (lookup-fn
 
 
+;;; group-by for 2 fields
+(let [i ittt
+      field-names '(:a :b)]
+  (magic-map (fn ([] (keys (i :a)))
+                 ([k1] (magic-map (fn ([] (map :b ((i :a) k1)))
+                                      ([k2] (clojure.set/intersection ((i :a) k1) ((i :b) k2)))))))))
 
+(let [i ittt
+      field-names '(:a :b)]
+  (magic-map (fn ([] (keys (i :a)))
+                 ([k] (let [index ((i :a) k)]
+                        (magic-map (fn ([] (map :b index))
+                                       ([k] (let [index (clojure.set/intersection ((i :b) k))]
+                                          (magic-map (fn ([] (map :c index))
+                                                         ([k] (let [index (clojure.set/intersection index ((i :c) k))]
+                                                                index)))))))))))))
+
+
+(defn clojure-index
+  "Returns an index as from clojure.set/index"
+  [R ks]
+  (let [gr (apply group-by R ks)]
+    (magic-map (fn ([] (tree-seq map? 
+
+
+(let [gr (group-by ttt :a :b :c)
+      f (fn [i cur & more] 
+          (map #(apply hash-map cur (key %))
+               i))]
+  (f gr '(:a :b :c))
+
+(2 1)
+
+(type (first (seq (group-by ttt :a :b :c))))
+
+
+(doc tree-seq)
+-------------------------
+clojure.core/tree-seq
+([branch? children root])
+  Returns a lazy sequence of the nodes in a tree, via a depth-first walk.
+   branch? must be a fn of one arg that returns true if passed a node
+   that can have children (but may not).  children must be a fn of one
+   arg that returns a sequence of the children. Will only be called on
+   nodes for which branch? returns true. Root is the root node of the
+  tree.
+nil
+
+ (doc clojure.set/index)
+-------------------------
+clojure.set/index
+([xrel ks])
+  Returns a map of the distinct values of ks in the xrel mapped to a
+  set of the maps in xrel with the corresponding values of ks.
+nil
+(group-by ttt :a :b)
+{2 {2 nil}, 1 {2 nil, 1 nil}}
+
+{2 #{{:a 2, :b 2, :c 3}}, 1 #{{:a 1, :b 2, :c 1} {:a 1, :b 1, :c 3}}}
+
+people
+#{{:id 7, :name unknown, :vorname unknown, :adress-id 104} {:id 2, :name kirsch, :vorname diana, :adress-id 100} {:id 6, :name zschieschang, :vorname mandy, :adress-id 103} {:id 5, :name soehnel, :vorname erik, :adress-id 103} {:id 4, :name hamann, :vorname robert, :adress-id 102} {:id 3, :name schmock, :vorname robert, :adress-id 101} {:id 1, :name weilandt, :vorname mathias, :adress-id 100}}
+
+
+(let [i ittt
+      field-names '(:a :b)]
+  ;;(map :b (vals 
+  ((i :a) 2))))
+
+;concat:
+(lazy-seq
+      (let [s (seq x)]
+        (if s
+          (cons (first s) (concat (rest s) y))
+          y))
 
 
 
@@ -140,4 +282,6 @@ B -> #{{:vorname diana, :name kirsch} {:vorname robert, :name schmock}}
 
 
 (pop (seq '(A v d)))
+
+
 
